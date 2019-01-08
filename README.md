5e5ab0673c068734d0f5cd86c814df581feef3fd
Big Mike <mikelawrence@users.noreply.github.com>
2019-01-08 15:48:18 -0600

    Initial commit
    Home Assistant custom components now have their own repository.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# homeassistant-custom-components
-Home Assistant Custom Components
+# Home Assistant Custom Components
+All my custom components for Home Assistant are in this repository.
+
+## Custom Haiku with SenseME fan and light
+The Haiku with SenseME fan is a WiFi connected fan and installable light. This custom component uses TomFaulkner's [SenseMe](https://github.com/TomFaulkner/SenseMe) library to communicate with the fan.
+
+### Installation
+There are three senseme.py files that must be installed in the config/custom_components directory. Note the location of the three senseme.py files in their respective folders (fan/ and light/) is important. The SenseMe library will be automatically installed by Home Assistant.
+
+### Configuration
+The Haiku with SenseME fan component will automatically discover and create a fan and light (if installed) for each discovered fan. Setting ```max_number_fans``` to the number of Haiku fans on your network will speed up the discovery process but is not required. If ```include:``` is specified, discovered fans with a matching name will be added. If ```exclude``` is specified, discovered fans with a matching name will NOT be added. If both ```include:``` and ```exclude``` are specified, only ```include:``` will be honored. If neither ```include:``` and ```exclude``` are specified, all auto-detected fans will be added.
+
+For included fans you can now specify a ```friendly_name``` to use instead of ```name``` in Home Assistant. This is handy for grouped fans. Controlling any fan in a group will affect all fans of that group. Default value is the same as ```name ```. Also new in the include section is the ```has_light``` boolean which when ```true``` will add a light component along with the fan. The default for ```has_light``` is ```true```. The included fan section must have a ```name ``` variable and it must must match the name in the Haiku app.
+```yaml
+# enable Haiku with SenseMe ceiling fans
+senseme:
+  max_number_fans: 2
+  # used to include only specific fans
+  include:
+    - name: "Studio Vault Fan"
+      friendly_name: "Studio Fan"
+      has_light: true
+    - name: "Family Room Fan"
+  # or use exclude to prevent specific auto-detected fan
+  exclude:
+    - "Studio Beam Fan"
+```
+
+### Problems
+* Occasionally changes to the fan state fail to connect to fan and make the change, usually as a network (python socket) error. Same thing is true for the SenseMe background task which gets the complete fan state every minute.
+* Originally the Senseme custom component auto-detected both the existence of a light and the fan's group but longer term usage showed a problem with consistently auto-detecting these values. This version no longer auto-detects these values and requires the user to specify them in advance.
+
+## Custom MQTT Sharing component
+In my Home Automation setup I use lots of SmartHome [Insteon](https://www.smarthome.com/insteon.html) devices controlled through an ISY-994i. This worked fine until I added another building to my property. Insteon devices support both RF and power line communication and neither worked consistently between the two buildings. I was also starting to add Z-Wave devices to my Home Automation system and it has the same problem between the two buildings. My solution was two instances of Home Assistant each with a Z-Stick for Z-Wave and an ISY-994i in each building. Now all my intra-building issues were solved but my attempts at using [MQTT Eventstream](https://www.home-assistant.io/components/mqtt_eventstream/) or [MQTT Statestream](https://www.home-assistant.io/components/mqtt_statestream/) to share data between multiple instances of Home Assistant left me wanting a solution that was a combination of both. With MQTT Eventstream events were shared (all mind you!) but entities were transient. If you restart Home Assistant all shared entities disappear until an event recreates the entity. With MQTT Statestream entity states are shared and retained but they are effectively read-only. What I really want is to share specified entities between two instances of Home Assistant where either instance can say turn a switch on and get state updates and receive events.
+
+More information to come...
---
+++ mqtt_shareclient.py
@@ -0,0 +1,124 @@
+"""
+Acts as a client to configured entities on MQTT. State changes are
+subscribed and call_service events are published. This version also receives
+isy994_control events for included entities.
+
+Primarily intended to share entities between multiple Home Assistant instances.
+
+For more details about this component, please refer to the documentation at
+https://github.com/mikelawrence/homeassistant-custom-components
+"""
+import asyncio
+import logging
+import json
+import voluptuous as vol
+
+from homeassistant.const import (MATCH_ALL, ATTR_ENTITY_ID, ATTR_STATE,
+    ATTR_SERVICE, ATTR_SERVICE_DATA, EVENT_CALL_SERVICE)
+from homeassistant.core import (callback, EventOrigin)
+from homeassistant.components.mqtt import (valid_publish_topic)
+from homeassistant.helpers.json import JSONEncoder
+import homeassistant.helpers.config_validation as cv
+
+CONF_BASE_TOPIC = 'base_topic'
+#CONF_CONTROL_TOPIC = 'control'      # not retained on MQTT server, to host
+#CONF_STATE_TOPIC = 'state'          # retained on MQTT server, from host
+#CONF_EVENT_TOPIC = 'event'          # not retained on MQTT server, from host
+
+DEPENDENCIES = ['mqtt']
+
+DOMAIN = 'mqtt_shareclient'
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_BASE_TOPIC): valid_publish_topic
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+_LOGGER = logging.getLogger(__name__)
+# all discovered entities
+entities = {}
+status_update = None
+
+@asyncio.coroutine
+def async_setup(hass, config):
+    """Set up MQTT Share Client."""
+    mqtt = hass.components.mqtt
+    conf = config.get(DOMAIN, {})
+    base_topic = conf.get(CONF_BASE_TOPIC)
+    if not base_topic.endswith('/'):
+        base_topic = base_topic + '/'
+    event_topic = base_topic + 'event'
+    control_topic = base_topic + 'control'
+    state_topic = base_topic + '+/+/state'
+
+    @callback
+    def _state_listener(topic, payload, qos):
+        """Receive remote states from mqtt_sharehost."""
+        # get the entity_id from the topic
+        split = topic.split('/')
+        domain = split[1]
+        object_id = split[2]
+        entity_id = domain + '.' + object_id
+        # process payload as JSON
+        values = json.loads(payload)
+        state = values.get(ATTR_STATE)
+        values.pop(ATTR_STATE)          # state is not an attribute
+        entities[entity_id] = True      # this was a remote status update
+        hass.states.async_set(entity_id, state, values)
+        # _LOGGER.warning("Received state for '{}', state={}, attributes={}".
+        #     format(entity_id, state, values))
+
+    # subscribe to all state topics
+    yield from mqtt.async_subscribe(state_topic, _state_listener)
+
+    @callback
+    def _control_publisher(event):
+        """Publish local call service events to mqtt_sharehost."""
+        # must be a local event
+        if event.origin != EventOrigin.local:
+            return
+        # must be a call_service event
+        if event.event_type != EVENT_CALL_SERVICE:
+            return
+        service_data = event.data.get(ATTR_SERVICE_DATA)
+        # entity_id in service_data can be a string or list of strings
+        #   force it to always be a list
+        if isinstance(service_data.get(ATTR_ENTITY_ID), list):
+            entity_ids = service_data.get(ATTR_ENTITY_ID)
+        else:
+            entity_ids = [service_data.get(ATTR_ENTITY_ID)]
+        # each entity_is published to its own topic
+        for entity_id in entity_ids:
+            # must be one of our entities
+            if not entity_id in entities:
+                break
+            # update entity_id
+            event.data[ATTR_SERVICE_DATA][ATTR_ENTITY_ID] = entity_id
+            event_info = {'event_type': event.event_type,
+                          'event_data': event.data}
+            payload = json.dumps(event_info, cls=JSONEncoder)
+            # publish the topic, retain should be off for events
+            hass.components.mqtt.async_publish(control_topic, payload, 0, False)
+            # _LOGGER.warning("Publish local control event '{}' data={}".format(
+            #     event.event_type, event_info))
+
+    # listen for local events if you are going to publish them.
+    hass.bus.async_listen(MATCH_ALL, _control_publisher)
+
+    @callback
+    def _event_listener(topic, payload, qos):
+        """Receive remote isy994_control events from mqtt_shareclient."""
+        # process payload as JSON
+        event = json.loads(payload)
+        event_type = event.get('event_type')
+        event_data = event.get('event_data')
+        # fire the event locally (origin is remote)
+        hass.bus.async_fire(event_type, event_data=event_data,
+                            origin=EventOrigin.remote)
+        # _LOGGER.warning("Received remote event '{}', data={}".format(event_type,
+        #     event_data))
+
+    # subscribe to all control topics
+    yield from mqtt.async_subscribe(event_topic, _event_listener)
+    return True
